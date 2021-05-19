d6e35b6204c3f965f62f678e2330a3692c2c52dc
Carlos Claro <carlosclaro79@gmail.com>
2021-05-19 19:08:35 -0300

    redução de divs

--- application/views/galeria_imovel.php
+++ application/views/galeria_imovel.php
@@ -1,45 +1,22 @@
 <div class="portlet light bordered">
     <div class="portlet-title tabbable-line">
-        <div class="caption">
-            <i class="icon-pin font-yellow-crusta"></i>
-            <h4 class="c-left c-font-18 c-font-bold c-font-uppercase caption-subject bold font-yellow-crusta uppercase"> Fotos e Vídeo </h4>
-            <div class="c-line-center c-theme-bg"></div>
-            <span class="caption-helper"></span>
-        </div>
+        <div class="caption"><i class="icon-pin font-yellow-crusta"></i><h4 class="c-left c-font-18 c-font-bold c-font-uppercase caption-subject bold font-yellow-crusta uppercase"> Fotos e Vídeo </h4><div class="c-line-center c-theme-bg"></div><span class="caption-helper"></span></div>
         <ul class="nav nav-tabs">
             <?php if ( ( isset($imovel->video) && !empty($imovel->video) ) ) : ?>
-            <li >
-                <a href="#video" data-toggle="tab"> Vídeo </a>
-            </li>
+            <li ><a href="#video" data-toggle="tab"> Vídeo </a></li>
             <?php endif; ?>
-            <li class="active">
-                <a href="#images_tab" data-toggle="tab"> Fotos ( <span class="foto-atual">1</span> de <span class="foto-qtde"><?php echo count($images['lista']['lista']);?></span> ) </a>
-            </li>
+            <li class="active"><a href="#images_tab" data-toggle="tab"> Fotos ( <span class="foto-atual">1</span> de <span class="foto-qtde"><?php echo count($images['lista']['lista']);?></span> ) </a></li>
         </ul>   
     </div>
-    <div class="portlet-body">
-        <div class="tab-content">
-            <div class="tab-pane active" id="images_tab">
-                <div class="carousel-master galeria-mobile carousel slide item-<?php echo $imovel->_id;?>" id="images">
-                    <div class="carousel-inner" aria-label="Carousel de imagens expandidas">
-                    <?php 
-                        $descricao_image = ( isset($images['lista']['principal']->titulo) && ! empty($images['lista']['principal']->titulo) ) ? $images['lista']['principal']->titulo : $imovel->imoveis_tipos_titulo;
-                        $image_gr = $images['lista']['principal']->arquivo_local;
-                        ?>
-                        <div class="item im active" data-ordem="0">
-                            <center><img itemprop="image" width="100%" height="auto" data-src="<?php echo $image_gr;?>" src="<?php echo base_url();?>imagens/naodisponivel.jpg" alt="<?php echo $descricao_image;?>" class="img-responsive primeira-image"></center>
-                        </div>
-                    </div>
-                    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev" data-item="<?php echo $imovel->_id;?>" data-atual="1"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Anterior</span></a>
-                    <a class="right carousel-control" href="#carousel" role="button" data-slide="next" data-item="<?php echo $imovel->_id;?>" data-atual="1"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Proximo</span></a>
-                    
-                </div>
-            </div>
-            <div class="tab-pane" id="video">
-                <?php if ( ( isset($imovel->video) && !empty($imovel->video) ) ) : ?>
-                    <center><iframe style="width:100%; height: auto; min-height: 550px;" class="video" src="<?php echo set_embed_video($imovel->video);?>" frameborder="0" allowfullscreen></iframe></center>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
+    <?php 
+    $descricao_image = ( isset($images['lista']['principal']->titulo) && ! empty($images['lista']['principal']->titulo) ) ? $images['lista']['principal']->titulo : $imovel->imoveis_tipos_titulo;
+    $image_gr = $images['lista']['principal']->arquivo_local;
+    ?>
+    <div class="portlet-body"><div class="tab-content"><div class="tab-pane active" id="images_tab"><div class="carousel-master galeria-mobile carousel slide item-<?php echo $imovel->_id;?>" id="images">
+            <div class="carousel-inner" aria-label="Carousel de imagens expandidas"><div class="item im active" data-ordem="0"><center><img itemprop="image" width="100%" height="auto" data-src="<?php echo $image_gr;?>" src="<?php echo base_url();?>imagens/naodisponivel.jpg" alt="<?php echo $descricao_image;?>" class="img-responsive primeira-image"></center></div></div>
+            <a class="left carousel-control" href="#carousel" role="button" data-slide="prev" data-item="<?php echo $imovel->_id;?>" data-atual="1"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Anterior</span></a>
+            <a class="right carousel-control" href="#carousel" role="button" data-slide="next" data-item="<?php echo $imovel->_id;?>" data-atual="1"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Proximo</span></a>
+        </div></div>
+    <div class="tab-pane" id="video"><?php if ( ( isset($imovel->video) && !empty($imovel->video) ) ) : ?><center><iframe style="width:100%; height: auto; min-height: 550px;" class="video" src="<?php echo set_embed_video($imovel->video);?>" frameborder="0" allowfullscreen></iframe></center><?php endif; ?></div>
+    </div></div>
 </div>
--- application/views/imovel_novo_3.php
+++ application/views/imovel_novo_3.php
@@ -196,45 +196,6 @@
                                                 </div>
                                                 <div class="form-group"><button class="btn btn-success envia ladda-button g-recaptcha" data-sitekey="<?php echo $sitekey;?>" type="submit" data-callback="onSubmit" data-action="submit"> Contatar Anunciante </button></div>
                                                 <div class="form-group"><label>&nbsp;Ao enviar, você concorda com os <a data-toggle="modal" data-target="#modal" class="termos" href="#">Termos de Uso</a>, <a data-toggle="modal" data-target="#modal" class="politica" href="#">Política de Privacidade</a> e recebimento de sugestões de imóveis.</label></div>
-                                                <ul class="list-unstyled list-inline">
-                                                    <?php
-                                                    $col= 6;
-                                                    $link_whats = '';
-                                                        if ( isset($item->imobiliaria_whatsapp) && ! empty($item->imobiliaria_whatsapp) ):
-                                                            $col = 4;
-                                                            ?>
-                                                            <li class="dropup">
-                                                                <div class="btn-group btn-vertical">
-                                                                    <button type="button" class="btn btn-form hidden-xs whats-desktop  btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                                        <i class="fa fa-whatsapp"></i> WhatsApp
-                                                                    </button>
-                                                                    <ul class="dropdown-menu">
-                                                                        <li>
-                                                                            <a target="_blank" class="btn ver-telefone-whats-lista"  href="https://web.whatsapp.com/send?phone=+55<?php echo $item->imobiliaria_whatsapp.'&text='. urlencode('Gostaria de saber mais sobre o imóvel: '.$url);?>" data-item="<?php echo $item->id;?>" data-log="<?php echo $origem;?>">Conversar com a imobiliária</a>
-                                                                        </li>
-                                                                        <li>
-                                                                            <a target="_blank" class="btn compartilha-telefone-whats-lista"  href="https://web.whatsapp.com/send?<?php echo '&text='. urlencode('Conheça esse imóvel: '.$url);?>" data-item="<?php echo $item->id;?>" data-log="<?php echo $origem;?>">Enviar para um amigo</a>
-                                                                        </li>
-                                                                    </ul>
-                                                                </div>
-                                                                <div class="btn-group btn-vertical">
-                                                                    <button type="button" class="btn btn-form whats-mobile  btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                                        <i class="fa fa-whatsapp">WhatsApp</i> 
-                                                                    </button>
-                                                                    <ul class="dropdown-menu">
-                                                                        <li><a target="_blank" class="btn ver-telefone-whats-lista" href="https://api.whatsapp.com/send?phone=+55<?php echo $item->imobiliaria_whatsapp.'&text='. urlencode('Gostaria de saber mais sobre o imóvel: '.$url);?>" data-item="<?php echo $item->id;?>" data-log="<?php echo $origem;?>">Conversar com a imobiliária</a></li>
-                                                                        <li><a target="_blank" class="btn compartilha-telefone-whats-lista"  href="https://api.whatsapp.com/send?<?php echo '&text='. urlencode('Conheça esse imóvel: '.$url);?>" data-item="<?php echo $item->id;?>" data-log="<?php echo $origem;?>">Enviar para um amigo</a></li>
-                                                                    </ul>
-                                                                </div>
-                                                            </li>
-                                                                    <?php
-                                                                endif;
-                                                                ?>
-                                                                </li>
-                                                                <li>
-                                                                    <a type="button" data-item="<?php echo $item->id_empresa;?>" class="btn btn-form btn-secondary  btn-topo" target="_blank" href="<?php echo base_url().'imobiliaria/'.urlencode( tira_especiais($item->imobiliaria_nome) ).'-'.$item->id_empresa;?>/" > + Imóveis <span class="hidden-xs">do anunciante</span></a>
-                                                                </li>
-                                                </ul>
                                             </div><!-- fecha coluna -->
                                         </div><!-- fecha row -->
                                     </form>
